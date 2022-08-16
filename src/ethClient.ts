1494f04bfa4b720103174ebc871b5f86d1632c28
hopeyen <hopeyen>
2022-08-16 11:30:51 -0700

    message validity: operator, stake, 2hr timestamp, no disputes

--- src/ethClient.ts
+++ src/ethClient.ts
@@ -1,17 +1,40 @@
+import {
+  registryABI,
+  registryAddress,
+} from "./registry-contract/registryContract";
 import { ethers } from "ethers";
 import { JsonRpcProvider } from "@ethersproject/providers";
 
 export class EthClient {
   provider: JsonRpcProvider;
+  goerliProvider: JsonRpcProvider;
+  registryContract: ethers.Contract;
 
   constructor() {
     const provider = new ethers.providers.JsonRpcProvider(
       `http://${process.env.ETH_NODE}`
     );
+    const goerliProvider = new ethers.providers.JsonRpcProvider(
+      process.env.GOERLI_NODE
+    );
     this.provider = provider;
+    this.goerliProvider = goerliProvider;
+    this.registryContract = new ethers.Contract(
+      registryAddress,
+      registryABI,
+      goerliProvider
+    );
   }
 
   async getBlockNumber(): Promise<number> {
     return await this.provider.getBlockNumber();
+  }
+
+  async checkRegistry(indexerAddress: string, operatorAddress: string) {
+    console.log(`check registry`, { contract: this.registryContract });
+    return await this.registryContract.operatorAuth(
+      indexerAddress,
+      operatorAddress
+    );
   }
 }
